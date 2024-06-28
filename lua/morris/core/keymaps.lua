b8f32aab5bb12538e2106a71872b76342fdc5afd
Maurice Koreman <mg.koreman99@gmail.com>
2024-06-28 14:02:58 +0200

    Add resizing splits keymaps

--- lua/morris/core/keymaps.lua
+++ lua/morris/core/keymaps.lua
@@ -19,6 +19,11 @@ keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window bottom" })
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window top" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase horizontal split size" })
+keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -5<CR>", { desc = "Decrease horizontal split size" })
+keymap.set("n", "<C-S-Up>", "<cmd>resize +5<CR>", { desc = "Increase vertical split size" })
+keymap.set("n", "<C-S-Down>", "<cmd>resize -5<CR>", { desc = "Decrease vertical split size" })
+
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
