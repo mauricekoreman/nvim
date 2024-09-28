98ad53a277c82f8b17541f09babb045880afcc82
Maurice Koreman <mg.koreman99@gmail.com>
2024-09-28 16:47:03 +0200

    idk

--- lua/morris/plugins/lualine.lua
+++ lua/morris/plugins/lualine.lua
@@ -1,21 +1,21 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-   -- local colors = {
-   --   blue = "#65D1FF",
-   --   green = "#3EFFDC",
-   --   violet = "#FF61EF",
-   --   yellow = "#FFDA7B",
-   --   red = "#FF4A4A",
-   --   fg = "#C3CCDC",
-   --   bg = "#112638",
-   --   inactive_bg = "#2C3043",
-   -- }
+		-- local colors = {
+		--   blue = "#65D1FF",
+		--   green = "#3EFFDC",
+		--   violet = "#FF61EF",
+		--   yellow = "#FFDA7B",
+		--   red = "#FF4A4A",
+		--   fg = "#C3CCDC",
+		--   bg = "#112638",
+		--   inactive_bg = "#2C3043",
+		-- }
 
-    lualine.setup()
-  end
+		lualine.setup({})
+	end,
 }
